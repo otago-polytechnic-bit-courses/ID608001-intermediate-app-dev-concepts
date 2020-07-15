23d628d7020de04176dc278e158ddc88c206dbb3
grayson-orr <graysono@op.ac.nz>
2020-07-16 00:25:16 +1200

    Add 09-django-3-forms-class-based-views.pdf & 10-django-4-template-inheritance-static-files.pdf

--- 09-django-3-forms-class-based-views/mvt/polls/views.py
+++ 09-django-3-forms-class-based-views/mvt/polls/views.py
@@ -1,22 +1,23 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 from .models import Question, Choice
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
-    return render(request, 'polls/detail.html', context)
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
-    return render(request, 'polls/results.html', context)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
