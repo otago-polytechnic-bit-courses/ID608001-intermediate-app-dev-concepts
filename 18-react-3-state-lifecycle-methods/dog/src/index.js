7863e7a164810161828d1f15b68e0fac58236783
Grayson Orr <GraysonO@op.ac.nz>
2020-08-26 10:24:06 +1200

    Update

--- 18-react-3-state-lifecycle-methods/dog/src/index.js
+++ 18-react-3-state-lifecycle-methods/dog/src/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+import Clock from './components/Clock'
 import Owner from './components/Owner'
 
 ReactDOM.render(
   <React.StrictMode>
     <Owner />
     <App />
+    <Clock />
   </React.StrictMode>,
   document.getElementById('root')
 )
