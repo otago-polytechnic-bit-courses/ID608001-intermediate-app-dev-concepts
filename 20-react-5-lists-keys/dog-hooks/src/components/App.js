83064159a4eb473b623decd183275215d6b3c26d
grayson-orr <graysono@op.ac.nz>
2020-09-15 12:42:31 +1200

    Update

--- 20-react-5-lists-keys/dog-hooks/src/components/App.js
+++ 20-react-5-lists-keys/dog-hooks/src/components/App.js
@@ -1,36 +1,35 @@
 import React from 'react'
-import Blog from './Blog'
+// import Blog from './Blog'
 import Clock from './Clock'
-import ListsKeys from './ListsKeys'
 import LoginControl from './LoginControl'
 import Owner from './Owner'
 import Register from './Register'
 import afghanHoundImg from '../img/afghan-hound.jpg'
-
-const dog = {
-  name: 'Bingo',
-  breed: 'Afghan Hound',
-  img: afghanHoundImg,
-}
-
-const numbers = [1, 2, 3, 4, 5]
-
-const posts = [
-  { id: 1, title: 'Hello World', content: 'Welcome to learning React!' },
-  { id: 2, title: 'Installation', content: 'You can install React from npm.' },
-]
-
-const formatDog = (dog) => `Woof woof, my name is ${dog.name} & my breed is an ${dog.breed}`
-
-const getGreeting = (dog) => {
-  if (dog) {
-    return <h1>{formatDog(dog)}</h1>
-  }
-  return <h1>Uh...who are you?</h1>
-}
-
+import NumberList from './NumberList'
 
 const App = () => {
+  const dog = {
+    name: 'Bingo',
+    breed: 'Afghan Hound',
+    img: afghanHoundImg,
+  }
+
+  const numbers = [1, 2, 3, 4, 5]
+
+  // const posts = [
+  //   { id: 1, title: 'Hello World', content: 'Welcome to learning React!' },
+  //   { id: 2, title: 'Installation', content: 'You can install React from npm.' },
+  // ]
+
+  const formatDog = (dog) =>
+    `Woof woof, my name is ${dog.name} & my breed is an ${dog.breed}`
+
+  const getGreeting = (dog) => {
+    if (dog) {
+      return <h1>{formatDog(dog)}</h1>
+    }
+    return <h1>Uh...who are you?</h1>
+  }
   return (
     <div className='main-container'>
       <Owner />
@@ -39,8 +38,8 @@ const App = () => {
       <img src={dog.img} alt='afghan hound' width='300' />
       <Clock />
       <LoginControl />
-      <ListsKeys numbers={numbers} />
-      <Blog posts={posts} />
+      <NumberList numbers={numbers} />
+      {/* <Blog posts={posts} /> */}
     </div>
   )
 }
