16319f053c4840d031dd12a2cfad3a407e82ece6
Grayson-Orr <grayson.orr@op.ac.nz>
2024-07-18 10:24:05 +1200

    update

--- lecture-notes/07-state-management/exemplar/src/utils/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartSliceReducer from "../slices/cartSlice";
-
-export const store = configureStore({
-  reducer: {
-    data: cartSliceReducer,
-  },
-});
--- lecture-notes/09-tailwind-css-and-shadcn-ui/exemplar/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-const App = () => {
-  return <Button>Click Me!</Button>;
-};
-
-export default App;
